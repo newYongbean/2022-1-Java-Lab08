eb2f8e62e5a65ec69d15100f2c96150f92164c93
yongbean <yongbeanchung@gmail.com>
2022-04-29 23:13:06 +0900

    sample folder

---
+++ app/src/main/java/Java/Lab08/Sample.java
@@ -0,0 +1,15 @@
+package Java.Lab08;
+
+public class Sample {
+	
+	public Sample() {
+		System.out.println("Sample message");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Sample();
+
+	}
+
+}
